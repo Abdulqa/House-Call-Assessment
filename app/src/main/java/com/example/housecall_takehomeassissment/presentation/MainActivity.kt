af38fcd44aa08514f54827ce9ebbe375df2748bb
Abdul Qadir <qadir.khatri@nymcard.com>
2025-05-04 09:20:11 +0500

    feat: screen navigation handling (Welocom, create account, login)

--- app/src/main/java/com/example/housecall_takehomeassissment/presentation/MainActivity.kt
+++ app/src/main/java/com/example/housecall_takehomeassissment/presentation/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.housecall_takehomeassissment.presentation.Routes.Companion.CREATE_ACCOUNT
+import com.example.housecall_takehomeassissment.presentation.Routes.Companion.LOGIN
+import com.example.housecall_takehomeassissment.presentation.Routes.Companion.WELCOME_SCREEN
+import com.example.housecall_takehomeassissment.presentation.navigation.screens.login.LoginScreen
+import com.example.housecall_takehomeassissment.presentation.navigation.screens.WelcomeScreen
+import com.example.housecall_takehomeassissment.presentation.navigation.screens.createaccount.CreateAccountScreen
 import com.example.housecall_takehomeassissment.presentation.theme.HouseCallTakeHomeAssissmentTheme
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,10 +24,37 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HouseCallTakeHomeAssissmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = WELCOME_SCREEN
+                ) {
+                    composable(WELCOME_SCREEN) {
+                        WelcomeScreen(navController)
+                    }
+                    composable(CREATE_ACCOUNT) {
+                        CreateAccountScreen(navController)
+                    }
+                    composable(LOGIN) {
+                        LoginScreen(navController)
+                    }
 
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        FirebaseAuth.getInstance().signOut()
+    }
+}
+
+class Routes {
+    companion object {
+        const val WELCOME_SCREEN = "welcome_screen"
+        const val CREATE_ACCOUNT = "create_account"
+        const val LOGIN = "login"
     }
 }
