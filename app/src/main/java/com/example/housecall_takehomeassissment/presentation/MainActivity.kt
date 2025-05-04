359893838952794bb3d2a3716fa2d278e66d626d
Abdul Qadir <qadir.khatri@nymcard.com>
2025-05-04 15:29:26 +0500

    ref: navigation details screen  route with passing required objects

--- app/src/main/java/com/example/housecall_takehomeassissment/presentation/MainActivity.kt
+++ app/src/main/java/com/example/housecall_takehomeassissment/presentation/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.housecall_takehomeassissment.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.housecall_takehomeassissment.data.models.ConceptProperty
 import com.example.housecall_takehomeassissment.presentation.Routes.Companion.CREATE_ACCOUNT
 import com.example.housecall_takehomeassissment.presentation.Routes.Companion.LOGIN
 import com.example.housecall_takehomeassissment.presentation.Routes.Companion.MEDICATION_DETAILS
@@ -21,6 +23,7 @@ import com.example.housecall_takehomeassissment.presentation.navigation.screens.
 import com.example.housecall_takehomeassissment.presentation.navigation.screens.medication.search.SearchMedicationScreen
 import com.example.housecall_takehomeassissment.presentation.theme.HouseCallTakeHomeAssissmentTheme
 import com.google.firebase.auth.FirebaseAuth
+import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -50,7 +53,23 @@ class MainActivity : ComponentActivity() {
                     composable(SEARCH_MEDICATION) {
                         SearchMedicationScreen(navController)
                     }
-                    composable(MEDICATION_DETAILS) {
+                    composable(
+                        MEDICATION_DETAILS.route, arguments = listOf(
+                            navArgument(MEDICATION_DETAILS.DETAILS_OBJ_KEY) {
+                                type = NavType.StringType
+                            },
+                            navArgument(MEDICATION_DETAILS.SHOW_ADD_TO_DB_BTN_KEY) {
+                                type = NavType.BoolType
+                            },
+                        )
+                    ) { backStackEntry ->
+                        val json =
+                            backStackEntry.arguments?.getString(MEDICATION_DETAILS.DETAILS_OBJ_KEY)
+                        val showAddBtn =
+                            backStackEntry.arguments?.getBoolean(MEDICATION_DETAILS.SHOW_ADD_TO_DB_BTN_KEY)
+                        val conceptProperty = json?.let {
+                            Gson().fromJson(it, ConceptProperty::class.java)
+                        }
                         MedicationDetailScreen(navController)
                     }
                 }
@@ -71,6 +90,17 @@ class Routes {
         const val LOGIN = "login"
         const val MY_MEDICATION = "my_medication"
         const val SEARCH_MEDICATION = "search_medication"
-        const val MEDICATION_DETAILS = "medication_details"
+
+        data object MEDICATION_DETAILS {
+            const val DETAILS_OBJ_KEY = "details_obj_key"
+            const val SHOW_ADD_TO_DB_BTN_KEY = "show_add_to_db_btn"
+            const val route =
+                "MEDICATION_DETAILS?$DETAILS_OBJ_KEY={$DETAILS_OBJ_KEY}&$SHOW_ADD_TO_DB_BTN_KEY={$SHOW_ADD_TO_DB_BTN_KEY}"
+
+            fun createRoute(obj: ConceptProperty, showAddBtn: Boolean): String {
+                val gsonString = Gson().toJson(obj)
+                return "MEDICATION_DETAILS?$DETAILS_OBJ_KEY=$gsonString&$SHOW_ADD_TO_DB_BTN_KEY=$showAddBtn"
+            }
+        }
     }
 }
