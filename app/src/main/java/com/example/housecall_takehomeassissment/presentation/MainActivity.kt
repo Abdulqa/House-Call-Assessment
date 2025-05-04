ff1c09608b157e408a06bb4795c4f50ed5c3b6d1
Abdul Qadir <qadir.khatri@nymcard.com>
2025-05-04 12:29:42 +0500

    added routes in navigation graph for screens (mymedicaiton, search, & details)

--- app/src/main/java/com/example/housecall_takehomeassissment/presentation/MainActivity.kt
+++ app/src/main/java/com/example/housecall_takehomeassissment/presentation/MainActivity.kt
@@ -9,10 +9,16 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.housecall_takehomeassissment.presentation.Routes.Companion.CREATE_ACCOUNT
 import com.example.housecall_takehomeassissment.presentation.Routes.Companion.LOGIN
+import com.example.housecall_takehomeassissment.presentation.Routes.Companion.MEDICATION_DETAILS
+import com.example.housecall_takehomeassissment.presentation.Routes.Companion.MY_MEDICATION
+import com.example.housecall_takehomeassissment.presentation.Routes.Companion.SEARCH_MEDICATION
 import com.example.housecall_takehomeassissment.presentation.Routes.Companion.WELCOME_SCREEN
-import com.example.housecall_takehomeassissment.presentation.navigation.screens.login.LoginScreen
 import com.example.housecall_takehomeassissment.presentation.navigation.screens.WelcomeScreen
 import com.example.housecall_takehomeassissment.presentation.navigation.screens.createaccount.CreateAccountScreen
+import com.example.housecall_takehomeassissment.presentation.navigation.screens.login.LoginScreen
+import com.example.housecall_takehomeassissment.presentation.navigation.screens.medication.detail.MedicationDetailScreen
+import com.example.housecall_takehomeassissment.presentation.navigation.screens.medication.mymedication.MyMedicationScreen
+import com.example.housecall_takehomeassissment.presentation.navigation.screens.medication.search.SearchMedicationScreen
 import com.example.housecall_takehomeassissment.presentation.theme.HouseCallTakeHomeAssissmentTheme
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,14 +44,22 @@ class MainActivity : ComponentActivity() {
                     composable(LOGIN) {
                         LoginScreen(navController)
                     }
-
+                    composable(MY_MEDICATION) {
+                        MyMedicationScreen(navController)
+                    }
+                    composable(SEARCH_MEDICATION) {
+                        SearchMedicationScreen(navController)
+                    }
+                    composable(MEDICATION_DETAILS) {
+                        MedicationDetailScreen(navController)
+                    }
                 }
             }
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onStart() {
+        super.onStart()
         FirebaseAuth.getInstance().signOut()
     }
 }
@@ -55,5 +69,8 @@ class Routes {
         const val WELCOME_SCREEN = "welcome_screen"
         const val CREATE_ACCOUNT = "create_account"
         const val LOGIN = "login"
+        const val MY_MEDICATION = "my_medication"
+        const val SEARCH_MEDICATION = "search_medication"
+        const val MEDICATION_DETAILS = "medication_details"
     }
 }
